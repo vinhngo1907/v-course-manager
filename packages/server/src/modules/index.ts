1709a850ab28affa6ad788959f7a56b2b13894b0
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-04-06 21:25:21 +0700

    user module

--- packages/server/src/modules/index.ts
+++ packages/server/src/modules/index.ts
@@ -4,11 +4,14 @@ import { Logger, MiddlewareConsumer, Module } from '@nestjs/common';
 import { DatabaseModule } from './database';
 import { AppConfigModule } from 'src/config';
 import { LoggerMiddleware } from 'src/common/middlewares/loggerMiddleware';
+import { UserModule } from './user';
 
 @Module({
 	imports: [
 		AppConfigModule,
-		DatabaseModule],
+		DatabaseModule,
+		UserModule
+	],
 	controllers: [],
 	providers: [Logger],
 })

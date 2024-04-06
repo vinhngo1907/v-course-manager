5e9eaefc3d8bd40ec33885121a81a96bfb302ce6
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-04-06 20:30:33 +0700

    init source

--- packages/server/src/modules/database/index.ts
+++ packages/server/src/modules/database/index.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+import { DatabaseService } from './service';
 
 @Module({
   providers: [DatabaseService],
--- packages/server/src/modules/index.ts
+++ packages/server/src/modules/index.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from '../app.controller';
-import { AppService } from '../app.service';
+// import { AppService } from '../app.service';
+import { DatabaseModule } from './database';
+import { AppConfigModule } from 'src/config';
+import { LoggerMiddleware } from 'src/common/middlewares/loggerMiddleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		AppConfigModule,
+		DatabaseModule],
+	controllers: [AppController],
+	providers: [],
 })
-export class AppModule {}
+export class AppModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggerMiddleware).forRoutes('*');
+	}
+}

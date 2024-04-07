9bda1d54b2595834fb6a5b8f9b05e3105aea3912
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-04-07 10:50:09 +0700

    commit

--- packages/server/src/modules/role/service.ts
+++ packages/server/src/modules/role/service.ts
@@ -1,8 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { IRole } from './types';
 import { DatabaseService } from '@modules/database/service';
 import { In } from 'typeorm';
+
 @Injectable()
 export class RoleService {
     private readonly logger: Logger = new Logger(RoleService.name);
@@ -10,11 +10,11 @@ export class RoleService {
         private readonly databaseService: DatabaseService
     ) { }
 
-    async getRolesByRolesStrArr(roles: String[]): Promise<IRole[]> {
-        return await this.databaseService.role.findMany({
-            where: {
-                name: In(roles)
-            }
-        })
+    async getRolesByRolesStrArr(roles: string[]): Promise<IRole[]> {
+        // return await this.databaseService.role.findMany({
+        //     where: {
+        //         name: In(roles)
+        //     }
+        // })
     }
 }

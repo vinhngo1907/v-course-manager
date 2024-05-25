fec0931727d9980a70c128b204e5167d09742fde
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-05-25 15:39:16 +0700

    commit

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
 				}),
 				/** 
 				 * - Write all logs with level `error` and below to `error.log`
-				 * git- Write all logs with level `info` and below to `combined.log`
+				 * - Write all logs with level `info` and below to `combined.log`
 				*/
 				new winston.transports.File({ filename: './src/common/logger/logs/error.log', level: 'error' }),
 				new winston.transports.File({ filename: './src/common/logger/logs/combined.log' }),

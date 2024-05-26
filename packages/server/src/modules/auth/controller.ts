ae7a150f37531d521cde7a6b6b84c2cf89ccf7c0
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-05-26 10:22:32 +0700

    comment api login

--- packages/server/src/modules/auth/controller.ts
+++ packages/server/src/modules/auth/controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, HttpCode, Post, Req, Res } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { ApiBody, ApiTags } from "@nestjs/swagger";
 import { Crud, CrudController } from "@nestjsx/crud";
 import { AuthService } from "./service";
 import RequestWithAccount from "./interfaces/RequestWithAccount";
 import { Response } from 'express';
-import { RegisterPayload } from "./types";
+import { LoginPayload, RegisterPayload } from "./types";
 import { Public } from "./decorator";
 
 @ApiTags('Auth')
@@ -14,16 +14,24 @@ export class AuthController {
         private authService: AuthService
     ) { }
 
-    @Post("login")
-    async login(@Req() req: RequestWithAccount, @Res() res: Response) {
-        // const { cookie, user: par}
-    }
+    // @HttpCode(200)
+    // @ApiBody(
+    //     type:LoginPayload
+    // )
+    // @Public()
+    // @UseGuards(LocalAuth)
+    // @Post("login")
+    // async login(
+    //     @Req() req: RequestWithAccount, 
+    //     @Res() res: Response) {
+    //     // const { cookie, user: par}
+    // }
 
     @Get("isLoggedIn")
     isLoggedIn(@Res() res: Response) {
         return res.send(true);
     }
-    
+
     @HttpCode(200)
     @Get('logout')
     logout(@Res() res: Response) {
@@ -38,7 +46,12 @@ export class AuthController {
     })
     @Public()
     @Post("reigster")
-    async register(@Body() registerPayload: RegisterPayload, @Res() res: Response) {
-        const {cookie, user} = await this.authService.register(registerPayload);
+    async register(
+        @Body() registerPayload: RegisterPayload,
+        @Res() res: Response
+    ): Promise<any> {
+        const { cookie, user } = await this.authService.register(registerPayload);
+        res.setHeader('Set-header', cookie);
+        return res.send(user);
     }
 }

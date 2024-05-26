1a9408699d2a8a408c9f4d51b13b4cf1e6831ef4
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-05-26 10:19:59 +0700

    api logout

--- packages/server/src/modules/auth/controller.ts
+++ packages/server/src/modules/auth/controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post, Req, Res } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, HttpCode, Post, Req, Res } from "@nestjs/common";
+import { ApiBody, ApiTags } from "@nestjs/swagger";
 import { Crud, CrudController } from "@nestjsx/crud";
 import { AuthService } from "./service";
 import RequestWithAccount from "./interfaces/RequestWithAccount";
 import { Response } from 'express';
 import { RegisterPayload } from "./types";
+import { Public } from "./decorator";
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -13,7 +14,7 @@ export class AuthController {
         private authService: AuthService
     ) { }
 
-    @Post("/login")
+    @Post("login")
     async login(@Req() req: RequestWithAccount, @Res() res: Response) {
         // const { cookie, user: par}
     }
@@ -22,8 +23,21 @@ export class AuthController {
     isLoggedIn(@Res() res: Response) {
         return res.send(true);
     }
+    
+    @HttpCode(200)
+    @Get('logout')
+    logout(@Res() res: Response) {
+        const emptyCookie = this.authService.getEmptyCookie();
+        res.setHeader('Set-cookie', emptyCookie);
+        return res.send();
+    }
 
-    @Post("/reigster")
+    @HttpCode(200)
+    @ApiBody({
+        type: RegisterPayload,
+    })
+    @Public()
+    @Post("reigster")
     async register(@Body() registerPayload: RegisterPayload, @Res() res: Response) {
         const {cookie, user} = await this.authService.register(registerPayload);
     }

2d48c4ed34034e5052b83d03404a83ff6dff1998
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-06-01 12:41:48 +0700

    server init

--- packages/server/src/modules/video/index.ts
+++ packages/server/src/modules/video/index.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VideoController } from './controller';
 import { VideoService } from './service';
+import { DatabaseService } from '@modules/database/service';
 
 @Module({
-  controllers: [VideoController],
-  providers: [VideoService]
+    controllers: [VideoController],
+    providers: [VideoService, DatabaseService]
 })
-export class VideoModule {}
+export class VideoModule { }

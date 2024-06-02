1f4c689ac3122845d1107fceb4ef714429a23505
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-06-02 11:05:43 +0700

    module - stream, comments_reaction

---
+++ packages/server/src/modules/stream/service.ts
@@ -0,0 +1,12 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { DatabaseService } from '@modules/database/service';
+import { VideoService } from '@modules/video/service';
+
+@Injectable()
+export class StreamService {
+    private readonly logger: Logger = new Logger(StreamService.name);
+    constructor(
+        private readonly databaseService: DatabaseService,
+        private readonly videoService: VideoService
+    ) { }
+}

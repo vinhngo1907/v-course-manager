b0237f75e0b3c005867359e212f5026328dc9366
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-09-01 14:52:05 +0700

    commit

--- packages/client/pages/settings/index.tsx
+++ packages/client/pages/settings/index.tsx
@@ -1,9 +1,18 @@
 // /pages/settings/index.tsx
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Layout from "@/Components/Layouts";
 import SettingsTabs from "@/Components/Settings/SettingsTabs";
+import { AuthContext } from "@/context/AuthContext";
 
 const SettingsPage: React.FC = () => {
+    const { authState: { isAuthenticated, user, authLoading } } = useContext(AuthContext)!;
+
+    useEffect(() => {
+        if (!authLoading && !isAuthenticated) {
+            // Redirect to login or show a message
+            window.location.href = '/'; // Redirect to home or login page
+        }
+    }, [isAuthenticated, authLoading]);
     return (
         <Layout title="Settings" isWide>
             <SettingsTabs />

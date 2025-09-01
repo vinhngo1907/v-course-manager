51056882c08721afacec3d31a5f0c596e38f95f7
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-09-01 15:06:33 +0700

    commit

--- packages/client/pages/settings/index.tsx
+++ packages/client/pages/settings/index.tsx
@@ -3,16 +3,28 @@ import React, { useContext, useEffect } from "react";
 import Layout from "@/Components/Layouts";
 import SettingsTabs from "@/Components/Settings/SettingsTabs";
 import { AuthContext } from "@/context/AuthContext";
+import { ModalContext } from "@/context/ModalContext";
 
 const SettingsPage: React.FC = () => {
     const { authState: { isAuthenticated, user, authLoading } } = useContext(AuthContext)!;
-
+    const { toggleModal } = useContext(ModalContext)!;
     useEffect(() => {
         if (!authLoading && !isAuthenticated) {
-            // Redirect to login or show a message
-            window.location.href = '/'; // Redirect to home or login page
+            toggleModal("login");
         }
-    }, [isAuthenticated, authLoading]);
+    }, [isAuthenticated, authLoading, toggleModal]);
+
+    if (!isAuthenticated) {
+        return (
+            <Layout title="Settings" isWide>
+                <p className="text-center text-gray-600">
+                   Please login to access Settings.
+                </p>
+            </Layout>
+        );
+    }
+
+
     return (
         <Layout title="Settings" isWide>
             <SettingsTabs />

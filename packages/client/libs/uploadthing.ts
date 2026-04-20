283ba2602fd364bfd06000f3d7a99fb9393e163d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2026-04-20 10:03:21 +0700

    fix(develop): update & upgrade

--- packages/client/libs/uploadthing.ts
+++
@@ -1,8 +0,0 @@
-// lib/uploadthing.ts
-import { generateReactHelpers, generateUploadDropzone } from "@uploadthing/react";
-import { OurFileRouter } from "@/pages/api/uploadthing/core";
-
-export const { uploadFiles, useUploadThing } =
-  generateReactHelpers<OurFileRouter>();
-
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
